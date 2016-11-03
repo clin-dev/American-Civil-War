5df196c271fc8bf4cd9802aaeb8067e8a719d5a2
chao1216 <chaolinle@gmail.com>
2016-11-03 16:59:30 -0500

    first commit

---
+++ heatMapDatas.js
@@ -0,0 +1,420 @@
+var group1 = {
+'battleOfFortSumter' : {'totalCasualty': 0,
+ 						 'lat':32.82,
+ 						 'lon':-79.90
+ 						},
+
+'firstBattleOfBullRun' : {'totalCasualty': 4690,
+ 						 'lat':38.81,
+ 						 'lon':-77.52
+						},
+
+'battleOfRichMountain' : {'totalCasualty': 364,
+ 						   'lat':38.87,
+ 						   'lon':-79.93
+ 						   },
+
+'battleOfBigBethel' : {'totalCasualty': 84,
+ 			  		   'lat': 37.093,
+ 			  		   'lon': -76.425
+ 			          },
+
+'battleOfBoonville' : {'totalCasualty': 10,
+ 			           'lat': 38.9765,
+ 			           'lon': -92.743
+ 			          },
+
+'battleOfCarthage' : {'totalCasualty': 244,
+ 			          'lat': 37.2,
+ 			          'lon': -94.34
+ 			         },
+
+'battleOfBlackburnFord' : {'totalCasualty': 151,
+ 			               'lat': 38.8,
+ 			               'lon': -77.44
+ 			              }
+};
+
+var group2 = {
+'battleOfWilsonsCreek' : {'totalCasualty': 2549,
+ 						   'lat':37.10,
+ 						   'lon':-93.41
+ 						   },
+
+'battleOfBallsBluff' : {'totalCasualty': 1157,
+ 						   'lat':39.13,
+ 						   'lon':-77.52
+ 						   },
+
+'battleOfCheatMountain' : {'totalCasualty': 178,
+ 						   'lat':38.62,
+ 						   'lon':-79.88
+ 						   },
+
+'battleOfCarnifexFerry' : {'totalCasualty': 188,
+ 						   'lat':38.21,
+ 						   'lon':-80.94
+ 						   },
+
+'battleOfLexington' : {'totalCasualty': 3267,
+ 			  		   'lat': 39.19,
+ 			  		   'lon': -93.87
+ 			          },
+
+'battleOfSantaRosaIsland' : {'totalCasualty': 154,
+ 			  			     'lat': 30.32,
+ 			  				 'lon': -87.28
+ 			     			},
+
+'battleOfCampWildCat' : {'totalCasualty': 78,
+ 			             'lat': 37.26,
+ 			             'lon': -84.20
+ 			            },
+
+'battleOfBelmont' : {'totalCasualty': 1248,
+ 			         'lat': 36.7657,
+ 			         'lon': -89.124
+ 			        }
+};
+
+
+var group3 = {
+'battleOfFortDonelson' : {'totalCasualty': 16537,
+ 						  'lat':36.49,
+ 						  'lon':-87.86
+ 						 },
+
+'battleOfIslandNumberTen' : {'totalCasualty': 7108,
+ 						   'lat':36.46,
+ 						   'lon':-89.47
+ 						   },
+
+'battleOfPeaRidge' : {'totalCasualty': 3384,
+ 						   'lat':36.45,
+ 						   'lon':-94.03
+ 						   },
+
+'battleOfGlorietaPass' : {'totalCasualty': 369,
+ 						   'lat':35.57,
+ 						   'lon':-105.75
+ 						   }, 	
+
+'battleOfValverde' : {'totalCasualty': 619,
+ 					  'lat':33.69,
+ 					  'lon':-106.91
+ 				     },
+
+'battleOfRoanokeIsland' : {'totalCasualty': 2907,
+ 						   'lat':35.88,
+ 						   'lon':-75.67
+ 						   },
+
+'battleOfFortHenry' : {'totalCasualty': 119,
+ 					   'lat':36.52,
+ 					   'lon':-88.04
+ 					  },
+
+'battleOfMillSprings' : {'totalCasualty': 775,
+ 						 'lat':37.06,
+ 						 'lon':-84.74
+ 						},
+
+'battleOfChustenahlah' : {'totalCasualty': 299,
+ 						   'lat':36.39,
+ 						   'lon':-96.01
+ 						   },
+
+'battleOfHamptonRoads' : {'totalCasualty': 464,
+ 						   'lat':36.98,
+ 						   'lon':-76.20
+ 						   },
+
+'battleOfNewBern' : {'totalCasualty': 1049,
+ 						   'lat':35.04,
+ 						   'lon':-77.01
+ 				    },
+
+'battleOfKErnstownFirst' : {'totalCasualty': 1308,
+ 						   'lat':39.14,
+ 						   'lon':-78.21
+ 						   },
+
+'battleOfCampAllegheny' : {'totalCasualty': 183,
+ 			               'lat': 38.47,
+ 			               'lon': -79.72
+ 			              },
+
+'battleOfDranesville' : {'totalCasualty': 301,
+ 			             'lat': 38.99,
+ 			             'lon': -77.33
+ 			            },
+
+'battleOfCockpitPoint' : {'totalCasualty': 50,
+ 			              'lat': 38.55,
+ 			              'lon': -77.26
+ 			             },
+
+'battleOfMiddleCreek' : {'totalCasualty': 93,
+ 			             'lat': 37.65,
+ 			             'lon': -82.81
+ 			            },
+
+'battleOfFortMacon' : {'totalCasualty': 450,
+ 			           'lat': 34.69,
+ 			           'lon': -76.67
+ 			          } 
+};
+
+
+var group4 = {
+'battleOfShiloh' : {'totalCasualty': 23746,
+ 				    'lat':35.15,
+ 				    'lon':-88.32
+				   },
+
+'battleOfSevenPines' : {'totalCasualty': 11165,
+ 						'lat':37.52,
+ 						'lon':-77.30
+ 					   },
+
+'battleOfGainesMill' : {'totalCasualty': 14830,
+ 						'lat':37.59,
+ 						'lon':-77.29
+ 					   },
+
+'battleOfMalvernHill' : {'totalCasualty': 8650,
+ 						 'lat':37.41,
+ 						 'lon':-77.25
+ 						},
+
+'battleOfChattanooga' : {'totalCasualty': 13824,
+ 						 'lat':35.05,
+ 						 'lon':-85.31
+					    },
+
+'battleOfFortsJacksonAndStPhilip' : {'totalCasualty': 1011,
+ 						             'lat':29.36,
+ 						             'lon':-89.46
+ 						            },
+
+'siegeOfCorinth' : {'totalCasualty': 2000,
+ 				    'lat':34.93,
+ 				    'lon':-88.52
+ 				   },
+
+'battleOfWinchesterFirst' : {'totalCasualty': 2419,
+ 						   'lat':39.18,
+ 						   'lon':-78.14
+ 						   },
+
+'battleOfYorktown' : {'totalCasualty': 482,
+ 					  'lat':37.18,
+ 				      'lon':-76.53
+ 				     },
+
+'battleOfFortPulaski' : {'totalCasualty': 380,
+ 						 'lat':32.03,
+ 						 'lon':-80.89
+ 						 },
+
+'battleOfWilliamsburg' : {'totalCasualty': 3965,
+ 						  'lat':37.26,
+ 						  'lon':-76.67
+ 						  },
+
+'battleOfDrewrysBluff' : {'totalCasualty': 39,
+ 						  'lat':37.42,
+ 						  'lon':-77.42
+ 						 }
+};
+
+
+var group5 = {
+'battleOfCedarMountain' : {'totalCasualty': 3691,
+ 						   'lat':38.40,
+ 						   'lon':-78.07
+ 						  	},
+
+'secondBattleOfBullRun' : {'totalCasualty':20300,
+ 						   'lat':38.81,
+ 						   'lon':-77.52
+ 						  	},
+
+'battleOfSouthMountain' : {'totalCasualty': 5010,
+ 						   'lat':39.48,
+ 						   'lon':-77.62
+ 						  },
+
+'battleOfAntietam' : {'totalCasualty': 25410,
+ 					  'lat':39.47,
+ 					  'lon':-77.74
+ 					 },
+
+'battleOfPerryvile' : {'totalCasualty': 7637,
+ 						'lat':37.68,
+ 						'lon':-84.97
+						},
+
+'battleOfStonesRiver' : {'totalCasualty': 24645,
+ 						 'lat':35.88,
+ 						 'lon':-86.44
+						},
+
+'battleOfFredericksburg' : {'totalCasualty': 18030,
+ 						  	'lat':38.30,
+ 						 	'lon':-77.47
+ 						 	}
+};
+
+
+var group6 = {
+'siegeOfVicksburg' : {'totalCasualty': 37532,
+ 				      'lat':32.34,
+ 				      'lon':-90.85
+ 					},
+
+'siegeOfPortHudson' : {'totalCasualty': 17500,
+ 					   'lat':30.69,
+ 					   'lon':-91.28
+ 					  },
+
+'battleOfGettysburg' : {'totalCasualty': 51049,
+						 'lat':39.81,
+ 						 'lon':-77.23
+ 						 },
+
+'battleOfChancellorsville' : {'totalCasualty': 30764,
+ 						   'lat':38.31,
+ 						   'lon':-77.65
+ 						   },
+
+'battleOfChampionHill' : {'totalCasualty': 6297,
+ 						   'lat':32.33,
+ 						   'lon':-90.53
+ 						   }
+};
+
+
+
+var group7 = {
+'battleOfChickamauga' : {'totalCasualty': 34624,
+ 						 'lat':34.92,
+ 						 'lon':-85.26
+						}
+};
+
+var group8 = {
+'battleOfWilderness' : {'totalCasualty': 28699,
+ 						'lat':38.32,
+ 						'lon':-77.76
+ 					   },
+
+'battleOfSpotsylvania' : {'totalCasualty': 31086,
+ 						  'lat':38.22,
+ 						  'lon':-77.60
+ 						  },
+
+'battleOfResaca' : {'totalCasualty': 7800,
+ 					'lat':34.58,
+ 					'lon':-84.94
+ 				   },
+
+'battleOfColdHarbor' : {'totalCasualty': 18025,
+ 						'lat':37.59,
+ 						'lon':-77.29
+ 						},
+
+'battleOfAtlanta' : {'totalCasualty': 9141,
+ 				     'lat':33.75,
+ 				     'lon':-84.35
+ 				    },
+
+'battleOfCrater' : {'totalCasualty': 5289,
+ 				    'lat':37.21,
+ 				    'lon':-77.37
+ 					},
+
+'battleOfMansfield' : {'totalCasualty': 3117,
+ 					   'lat':32.01,
+ 					   'lon':-93.67
+ 					  },
+
+'battleOfPetersburgSecond' : {'totalCasualty': 15386,
+ 						   'lat':37.22,
+ 						   'lon':-77.38
+ 						   }
+};
+
+var group9 = {
+'battleOfGlobeTavern' : {'totalCasualty': 5916,
+ 						 'lat':37.17,
+ 						 'lon':-77.42
+ 						},
+
+'battleOfJonesborough' : {'totalCasualty': 3849,
+ 						  'lat':33.53,
+ 						  'lon':-84.36
+						},
+
+'battleOfCedarCreek' : {'totalCasualty': 8575,
+ 						'lat':39.02,
+ 						'lon':-78.30
+ 						},
+
+'battleOfFranklin' : {'totalCasualty': 8578,
+ 					  'lat':35.91,
+ 				      'lon':-86.87
+ 					 },
+
+'battleOfOpequon' : {'totalCasualty': 8630,
+ 				     'lat':39.20,
+ 				     'lon':-78.13
+ 				    },
+
+'battleOfWestport' : {'totalCasualty': 3000,
+ 					  'lat':39.03,
+ 					  'lon':-94.59
+ 				     }
+};
+
+var group10 = {
+'battleOfNashville' : {'totalCasualty': 9061,
+ 					   'lat':36.08,
+ 					   'lon':-86.80
+ 					  },
+
+'battleOfBentonville' : {'totalCasualty': 4133,
+ 						 'lat':35.25,
+ 						 'lon':-78.67
+ 						},
+
+'battleOfFiveForks' : {'totalCasualty': 3780,
+ 					   'lat':37.13,
+ 					   'lon':-77.62
+ 					  },
+
+'battleOfFortFisher' : {'totalCasualty': 2957,
+ 						'lat':33.97,
+ 						'lon':-77.92
+ 					   },
+
+'battleOfFortStedman' : {'totalCasualty': 5044,
+ 						 'lat':37.23,
+ 						 'lon':-77.37
+ 						},
+
+'battleOfPetersburgThird' : {'totalCasualty': 8936,
+ 						   'lat':37.18,
+ 						   'lon':-77.45
+ 						   },
+
+'battleOfFortBlakely' : {'totalCasualty': 3529,
+ 						   'lat':30.74,
+ 						   'lon':-87.93
+ 						},
+
+'battleOfSailorsCreek' : {'totalCasualty': 8848,
+ 						  'lat':37.30,
+ 						  'lon':-78.22
+ 						 }
+};
